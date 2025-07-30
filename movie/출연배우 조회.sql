8cf91aae61c127b64affb9150cdeda210bd924dd
Yujin <wjd2000db@gmail.com>
2025-07-30 18:27:26 +0900

    컨벤션 적용

--- movie/출연배우 조회.sql
+++ movie/출연배우 조회.sql
@@ -6,12 +6,12 @@ CREATE OR REPLACE PROCEDURE searchCasts(
 )
 BEGIN
 	SELECT 
-	     b.name AS '출연 배우'
-	FROM movie_actors a
-	JOIN actor b ON a.actor_code = b.code
-	JOIN movie c ON a.movie_code = c.code
-	WHERE c.name = title
-	ORDER BY c.name;	
+	       b.name AS '출연 배우'
+	  FROM movie_actors a
+	  JOIN actor b ON a.actor_code = b.code
+	  JOIN movie c ON a.movie_code = c.code
+	 WHERE c.name = title
+	 ORDER BY c.name;	
 END // 
 
 DELIMITER ;
