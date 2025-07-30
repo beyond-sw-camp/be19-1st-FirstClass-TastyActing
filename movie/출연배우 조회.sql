01ee0cad2cc2595238ddab457cccfe9d890f2d36
Yujin <wjd2000db@gmail.com>
2025-07-30 16:39:05 +0900

    영화 출연 배우 조회

---
+++ movie/출연배우 조회.sql
@@ -0,0 +1,21 @@
+-- 영화 출연배우 조회
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE viewCasts(
+	IN title VARCHAR(100)
+)
+BEGIN
+	SELECT 
+	     b.name AS '출연 배우'
+	FROM movie_actors a
+	JOIN actor b ON a.actor_code = b.code
+	JOIN movie c ON a.movie_code = c.code
+	WHERE c.name = title
+	ORDER BY c.name;	
+END // 
+
+DELIMITER ;
+
+CALL viewCasts('퍼스트클래스'); 
+
+
