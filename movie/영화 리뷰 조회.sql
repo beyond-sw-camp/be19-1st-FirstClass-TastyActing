8a239c150b21186bb8ed88d0406d830ecbc55dc7
Yujin <wjd2000db@gmail.com>
2025-07-31 16:32:01 +0900

    영화 리뷰 작성자코드 -> 이름 출력으로 변경

--- movie/영화 리뷰 조회.sql
+++ movie/영화 리뷰 조회.sql
@@ -29,13 +29,14 @@ CREATE OR REPLACE PROCEDURE searchReviewByMovie(
 )
 BEGIN
 	SELECT 
-		   a.user_code AS '작성자'
+		   c.name AS '작성자'
 		 , a.score AS '평점'
 	 	 , a.content AS '내용'
 	 	 , a.date AS '작성일'
 	 	 , a.number AS '좋아요 개수'
 	  FROM movie_review a
-	  JOIN movie b ON a.movie_code = b.code;
+	  JOIN movie b ON a.movie_code = b.code
+	  JOIN user c ON a.user_code = c.code;
 END //
 
 DELIMITER ;
