50238c1d531c3ff3134e2b50838ae00746e4ab8c
Yujin <wjd2000db@gmail.com>
2025-07-31 18:13:04 +0900

    리뷰 코드 추가

--- movie/영화 리뷰 댓글 조회.sql
+++ movie/영화 리뷰 댓글 조회.sql
@@ -1,4 +1,4 @@
--- 영화 이름으로 검색해서 해당 영화의 모든 댓글 조회
+-- 검색된 영화의 모든 댓글 조회
 DELIMITER //
 
 CREATE OR REPLACE PROCEDURE searchCommentByReview(
@@ -9,6 +9,7 @@ BEGIN
 		   a.content AS '내용'
 		 , c.name AS '작성자'
 		 , a.date AS '작성날짜'
+		 , b.code AS '리뷰 코드'
 	  FROM comment a
 	  JOIN movie_review b ON a.movie_review_code = b.code
 	  JOIN user c ON a.user_code = c.code
