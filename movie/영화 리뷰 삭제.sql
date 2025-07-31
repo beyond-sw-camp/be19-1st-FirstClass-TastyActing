f565c3a2f70eed04073dcf11418d7766e20323cf
Yujin <wjd2000db@gmail.com>
2025-07-31 10:22:43 +0900

    영화 리뷰 CRUD 파일 및 평점 계산

---
+++ movie/영화 리뷰 삭제.sql
@@ -0,0 +1,20 @@
+-- 영화 리뷰 삭제 
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE deleteMovieReview(
+    IN user INTEGER,
+    IN movie INTEGER
+)
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM movie_review 
+        WHERE user_code = user AND movie_code = movie
+    ) THEN
+        DELETE FROM movie_review
+        WHERE user_code = user AND movie_code = movie;
+    END IF;
+END //
+
+DELIMITER ;
+
+CALL deleteMovieReview(1,15);
