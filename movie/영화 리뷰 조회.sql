e8e9bdc7f85331155cc2a8ea78e84f5b136f8376
Yujin <wjd2000db@gmail.com>
2025-07-31 16:57:21 +0900

    중복되는 쿼리 제거

--- movie/영화 리뷰 조회.sql
+++ movie/영화 리뷰 조회.sql
@@ -1,26 +1,3 @@
--- 사용자가 작성한 영화 리뷰 조회
-DELIMITER //
-
-CREATE OR REPLACE PROCEDURE searchReviewByUser(
-	IN user_code INTEGER
-)
-BEGIN
-	SELECT 
-	   b.name AS '영화 제목'
-	 , a.score AS '평점'
- 	 , a.content AS '내용'
- 	 , a.date AS '작성일'
- 	 , a.number AS '좋아요 개수'
-  FROM movie_review a
-  JOIN movie b ON a.movie_code = b.code
- WHERE a.user_code = user_code;
-END //
-
-DELIMITER ;
-
-CALL searchReviewByUser(4);
-
-
 -- 영화별 리뷰 조회
 DELIMITER //
 
