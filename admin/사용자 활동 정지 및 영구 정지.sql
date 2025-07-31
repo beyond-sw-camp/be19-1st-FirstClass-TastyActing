b2a86b8e0bca50614e42bd8f74999b6969f5b54c
woo-kyoung-nam <dnrud6911@gmail.com>
2025-07-31 18:02:39 +0900

    관리자_사용자 관리

---
+++ admin/사용자 활동 정지 및 영구 정지.sql
@@ -0,0 +1,30 @@
+-- 사용자 활동 정지 및 영구 정지
+-- 사용자 활동 정지(15번 신고된 사용자 활동 정지, warning으로 상태변경 및 처리일자(현재일자)부터 30일 뒤를 정지 만료일로 설정)
+-- 사용자 영구 정지(30번 신고된 사용자 강제 탈퇴,banned으로 상태변경 및 blacklist 테이블에 강제 탈퇴 사용자 삽입)
+
+delimiter //
+
+CREATE OR REPLACE TRIGGER user_status_control
+BEFORE UPDATE ON user
+FOR EACH ROW
+BEGIN
+    -- 신고 30회: 계정 영구 정지 및 블랙리스트 추가
+    IF NEW.report_count = 30 THEN
+       
+        IF OLD.name IS NOT NULL AND OLD.my_number IS NOT NULL AND NEW.status != 'banned' THEN
+            INSERT INTO blacklist (name, my_number)
+            VALUES (OLD.name, OLD.my_number);
+        END IF;
+        
+        SET NEW.status = 'banned';
+        SET NEW.expires_at = NULL;
+
+    -- 신고 15회: 30일 정지
+    ELSEIF NEW.report_count = 15 THEN
+        SET NEW.status = 'warning';
+        SET NEW.expires_at = DATE_ADD(CURDATE(), INTERVAL 30 DAY);     -- 오늘 날짜로부터 30일 뒤를 정지 만료일을 설정
+    END IF;
+END;
+//
+
+delimiter ;
---
+++ admin/활동 정지 사용자 해제.sql
@@ -0,0 +1,7 @@
+-- 활동 정지 사용자 해제(처리일자(CURDATE()사용함)로부터 30일 이후 활동 정지된 사용자 해제)
+-- 상태가 warning이고 정지해제일은 null값이 아니고 현재날짜가 정지해제일보다 크면 상태를 normal로 업데이트
+UPDATE user
+   SET  = 'normal'
+ WHERE status = 'warning'
+   AND expires_at IS NOT NULL
+   AND CURDATE() > expires_at;
