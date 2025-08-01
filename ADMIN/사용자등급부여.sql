8cc5786d4a1545ad97dcca49483bb41fd5e425db
bynmch <byunmch@naver.com>
2025-08-01 10:19:55 +0900

    사용자등급부여 pr 수정 및 stored function 수정

--- ADMIN/사용자등급부여.sql
+++ ADMIN/사용자등급부여.sql
@@ -12,13 +12,13 @@ BEGIN
    DECLARE total_likes INTEGER;
 
    SELECT
-          IFNULL(SUM(mr.number), 0) +
-          IFNULL(SUM(ar.number), 0)
+          IFNULL(SUM(b.like_count), 0) +
+          IFNULL(SUM(c.like_count), 0)
      INTO total_likes
-     FROM user u
-     LEFT JOIN movie_review mr ON u.code = mr.user_code
-     LEFT JOIN actor_review ar ON u.code = ar.user_code
-    WHERE u.code = t_code;
+     FROM user a
+     LEFT JOIN movie_review b ON a.code = b.user_code
+     LEFT JOIN actor_review c ON a.code = c.user_code
+    WHERE a.code = t_code;
 
    RETURN total_likes;
 END //
@@ -78,60 +78,8 @@ END //
 
 delimiter ;
 
-
--- 샘플 데이터 (movie_review)
-INSERT 
-  INTO movie_review
-(
-  code
-, score
-, date
-, content
-, report_count
-, number
-, user_code
-, movie_code
-)
-VALUES
-  (1, 4.2, NOW(), '정말 감동적이었어요.', 1, 30, 1, 1)
-, (2, 5.0, NOW(), '액션이 살아있네.', 2, 25,2, 2)
-, (3, 3.2, NOW(), '웃기고 재미있어요.', 3, 60, 3, 3)
-, (4, 4.5, NOW(), '배우 연기가 최고입니다.', 4, 40, 4, 4)
-, (5, 2.5, NOW(), '배우 연기가 별론데예.', 4, 10, 5, 5)
-, (6, 4.1, NOW(), '배우 연기가 별론데예.', 4, 10, 6, 6)
-, (7, 3.7, NOW(), '웃기고 재미있어요.', 3, 60, 7, 7)
-, (8, 4.5, NOW(), '배우 연기가 최고입니다.', 4, 40, 8, 8)
-, (9, 2.5, NOW(), '배우 연기가 별론데예.', 4, 10, 9, 9)
-, (10, 4.1, NOW(), '배우 연기가 별론데예.', 4, 10, 10, 10);
-
--- 샘플 데이터 (actor_review)
-INSERT
-INTO actor_review
-(
-  code
-, score
-, content
-, date
-, report_count
-, number
-, movie_code
-, actor_code
-, user_code
-)
-VALUES
-  (1, 4, '송강호 연기 최고예요.', NOW(), 1, 10, 1, 1, 1)
-, (2, 5, '박소담 연기 너무 좋아요.', NOW(), 2, 45, 2, 2, 2)
-, (3, 3, '최우식 배우 눈빛 연기 굿.', NOW(), 3, 80, 3, 3, 3)
-, (4, 4, '송강호 연기 최고예요.', NOW(), 4, 10, 4, 4, 4)
-, (5, 5, '박소담 연기 너무 좋아요.', NOW(), 5, 10, 5, 5, 5)
-, (6, 3, '최우식 배우 눈빛 연기 굿.', NOW(), 6, 10, 6, 6, 6)
-, (7, 3, '최우식 배우 눈빛 연기 굿.', NOW(), 3, 80, 7, 7, 7)
-, (8, 4, '송강호 연기 최고예요.', NOW(), 4, 10, 8, 8, 8)
-, (9, 5, '박소담 연기 너무 좋아요.', NOW(), 5, 10, 9, 9, 9)
-, (10, 3, '최우식 배우 눈빛 연기 굿.', NOW(), 6, 10, 10, 10, 10);
-
 -- 트리거확인을 위한 좋아요 수 수정
-UPDATE movie_review SET number = 50 WHERE code = 1;
+UPDATE movie_review SET like_count = 50 WHERE code = 1;
 
 -- 실행 테스트
 CALL user_like_level(1);
@@ -147,18 +95,18 @@ CALL user_like_level(10);
 
 -- 결과 확인
 SELECT
-       u.code AS '회원코드'
-     , u.id AS 'ID'
-     , u.pw AS 'PW'
-     , u.name AS '회원명'
-     , u.birth_date AS '생년월일'
-     , u.status AS '회원상태'
-     , u.expires_at AS '정지해제일'
-     , u.report_count AS '신고횟수'
-     , u.like_count AS '좋아요수'
-     , u.id_number AS '주민등록번호'
-     , r.user_role AS '권한명'
-     , l.name AS '등급명'
-  FROM user u
-  LEFT JOIN role r ON u.role_code = r.code
-  LEFT JOIN level l ON u.level_code = l.code ;
+       a.code AS '회원코드'
+     , a.id AS 'ID'
+     , a.pw AS 'PW'
+     , a.name AS '회원명'
+     , a.birth_date AS '생년월일'
+     , a.status AS '회원상태'
+     , a.expires_at AS '정지해제일'
+     , a.report_count AS '신고횟수'
+     , a.like_count AS '좋아요수'
+     , a.id_number AS '주민등록번호'
+     , b.user_role AS '권한명'
+     , c.name AS '등급명'
+  FROM user a
+  LEFT JOIN role b ON a.role_code = b.code
+  LEFT JOIN level c ON a.level_code = c.code ;
