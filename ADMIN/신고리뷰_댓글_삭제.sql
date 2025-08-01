976dbf6e805c0ea49f12dfedf97a4d3025020ede
bynmch <byunmch@naver.com>
2025-08-01 11:27:05 +0900

    신고누적 영화리뷰 삭제

--- ADMIN/신고리뷰_댓글_삭제.sql
+++ ADMIN/신고리뷰_댓글_삭제.sql
@@ -1,63 +1,30 @@
 -- 리뷰 및 댓글 삭제(신고5회 이상 받은)
 
--- -- 영화 리뷰 신고 시 실행할 로직 (가상 코드)
--- 1단계: 먼저 report_count를 1 증가시킵니다.
-UPDATE movie_review
-   SET report_count = report_count + 1
- WHERE code = 1;
+delimiter //
+CREATE OR REPLACE TRIGGER trg_delete_movie_review
+AFTER UPDATE ON movie_review
+FOR EACH ROW
+BEGIN
+    IF NEW.report_count >= 5 AND 
+       EXISTS (
+           SELECT 1
+             FROM report
+            WHERE category_code = 1
+              AND movie_review_code = NEW.code
+              AND is_processed = 0
+       ) THEN
+        DELETE FROM movie_review
+        WHERE code = NEW.code;
+    END IF;
+END //
 
--- 2단계: 업데이트된 report_count 값을 다시 조회합니다.
-SELECT report_count
-  FROM movie_review
- WHERE code = 1;
+delimiter ;
 
--- 3단계: 조회한 report_count가 5 이상이면, 해당 리뷰를 삭제합니다.
--- (애플리케이션 코드에서 if 문으로 처리)
-IF (report_count >= 5) THEN
-    DELETE FROM movie_review
-     WHERE code = 1;
-END IF;
-
--- ---------------------------------------------
--- 배우 리뷰 신고 시 실행할 로직 (가상 코드)
--- 1단계: report_count를 1 증가시킵니다.
-UPDATE actor_review
-   SET report_count = report_count + 1
- WHERE CODE = 1;
-
--- 2단계: 업데이트된 report_count를 조회합니다.
-SELECT report_count
-  FROM actor_review
- WHERE CODE = 1;
-
--- 3단계: 조회한 report_count가 5 이상이면, 해당 리뷰를 삭제합니다.
--- (애플리케이션 코드에서 if 문으로 처리)
-IF (report_count >= 5) THEN
-    DELETE FROM actor_review
-     WHERE CODE = 1;
-END IF;
+UPDATE movie_review SET report_count = 5 WHERE CODE = 2 ;   
 
 -- ------------------------------------------------------------
--- comment 신고 시 실행할 로직 (가상 코드)
--- 1단계: report_count를 1 증가시킵니다.
-UPDATE comment
-   SET report_count = report_count + 1
- WHERE code = 1;
-
--- 2단계: 업데이트된 report_count를 조회합니다.
-SELECT report_count
-  FROM comment
- WHERE CODE = 1;
-
--- 3단계: 조회한 report_count가 5 이상이면, 해당 댓글을 삭제합니다.
--- (애플리케이션 코드에서 if 문으로 처리)
-IF (report_count >= 5) THEN
-    DELETE FROM comment
-     WHERE CODE = 1;
-END IF;
-
-  
-
-
+SELECT * FROM movie_review;
+SELECT * FROM report;
+SELECT * FROM category;
 
 
