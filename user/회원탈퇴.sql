5e671c572e30f599f2dff83f5b49f8e997aa2f50
SoonWooLee1 <lsw010420@gmail.com>
2025-08-01 15:30:49 +0900

    회원탈퇴 기능에서 탈퇴하면 status = deleted로 바뀌게 설정했습니다.

--- user/회원탈퇴.sql
+++ user/회원탈퇴.sql
@@ -3,11 +3,13 @@ SELECT * FROM user;   -- 회원 코드 확인
 -- 회원 탈퇴
 DELIMITER //
 
-CREATE OR REPLACE PROCEDURE delete_user_cascade(IN in_user_code INT)
+CREATE OR REPLACE PROCEDURE delete_user(IN in_user_code INT)
 BEGIN
-    DELETE FROM `user` WHERE code = in_user_code;
+    UPDATE user
+       SET STATUS = 'deleted'
+     WHERE code = in_user_code;
 END//
 
 DELIMITER ;
 
-CALL delete_user_cascade(1);
+CALL delete_user(1);
